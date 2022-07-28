20ea311c7083b95203e4f6d01d930bfeec46b5f7
Perry Vangronsveld <perry.vangronsveld@signify.com>
2022-07-28 13:17:06 +0200

    Added ProductList.vue

--- App.vue
+++ App.vue
@@ -2,15 +2,17 @@
   <div id="app">
     <hello-world/>
     <click-counter />
+    <product-list title="Testing" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./src/components/HelloWorld.vue";
 import ClickCounter from "./src/components/ClickCounter.vue";
+import ProductList from "./src/components/ProductList.vue";
 export default {
   name: "App",
-  components: {ClickCounter, HelloWorld},
+  components: {ProductList, ClickCounter, HelloWorld},
 }
 </script>
 
---
+++ src/components/ProductList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="k-product-list">
+    <h2 class="k-product-list__header">{{title}}</h2>
+    <ul>
+      <li v-for="(item, idx) in items" :key="idx" class="k-product-item">
+        <img :src="item.imageUrl" class="k-product-item__image">
+        <h4 class="k-product-item__title">{{ item.title }}</h4>
+        <button @click="removeItem(item)">Remove Item</button>
+      </li>
+    </ul>
+    <button @click="addItem">Add Item</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push({ title: 'New Serie', imageUrl: 'https://via.placeholder.com/400' })
+    },
+    removeItem(item) {
+      const index = this.items.indexOf(item);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+    }
+  }
+}
+</script>
